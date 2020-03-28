fcd0ace62d01bef507b843c23c4e0c3cae28cb36
jimenalozano <jilzoano@itba.edu.ar>
2020-03-28 02:12:34 -0300

    hungarian terminado. no lo probé

--- TP1/src/com/company/Heuristics.java
+++ TP1/src/com/company/Heuristics.java
@@ -1,17 +1,14 @@
 package com.company;
 
-import javafx.util.Pair;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Heuristics {
 
-    private Integer INFINITE_COST = 1000000000;
+    private AtomicInteger INFINITE_COST = new AtomicInteger(1000000000);
     private ArrayList<Integer[]> goalCoordinates = new ArrayList<>();
     private ArrayList<Integer[]> boxCoordinates = new ArrayList<>();
-    private Map<Integer[], Map<Integer[], Integer>> matrix = new HashMap<>();
+    private Integer[][] hungarianMatrix;
 
     /*
     en esta heurística la evaluación es sobre la cercanía de las cajas a los goals
@@ -105,42 +102,142 @@ public class Heuristics {
     public int minimumMatchingLowerBound (Board board) {
 
         getCoordinates(board);
-        int movesCount = INFINITE_COST;
-        for (Integer[] boxCoordinate : boxCoordinates ){
-            Map<Integer[], Integer> goalColumns = matrix.put(boxCoordinate, new HashMap<>());
-            for (Integer[] goalCoordinate : goalCoordinates) {
-                movesCount = movesCounter(board, boxCoordinate, goalCoordinate);
-                if (goalColumns != null) goalColumns.put(goalCoordinate, movesCount);
+        hungarianMatrix = new Integer[boxCoordinates.size()][goalCoordinates.size()];
+        for (int i = 0; i < boxCoordinates.size(); i++){
+            for (int j = 0; j < goalCoordinates.size(); j++) {
+                hungarianMatrix[i][j] = movesCounter(board, boxCoordinates.get(i), goalCoordinates.get(j));
             }
         }
 
-        /* elegir los mínimos del mapa y sumar */
-        return -1;
+        Integer[] combinations = new Integer[(int) Math.pow(boxCoordinates.size(), goalCoordinates.size())];
+        for (int i = 0; i < boxCoordinates.size(); i++) {
+            for (int j = 0; j < goalCoordinates.size(); j++) {
+
+                int range = (int) Math.pow(boxCoordinates.size(), goalCoordinates.size() - (i+1));
+                int space;
+                if (i == 0) space = 0;
+                else {
+                    space = (int) Math.pow(boxCoordinates.size(), goalCoordinates.size() - i);
+                }
+                int rep = 0;
+
+                do {
+                    for (int k = j * range + rep * space; k < (j+1) * range + rep * space ; k++) {
+                        combinations[k] += hungarianMatrix[i][j];
+                    }
+                    rep++;
+                } while (rep < i*boxCoordinates.size());
+
+            }
+        }
+
+        int resp = INFINITE_COST.get();
+
+        for (int i = 0; i < boxCoordinates.size() * goalCoordinates.size(); i++) {
+            resp = Math.min(resp, combinations[i]);
+        }
+
+        return resp;
     }
 
     private int movesCounter (Board board, Integer[] from, Integer[] to) {
 
         char[][] b = board.getBoard();
-        String[] directions = new String[] {"UP", "DOWN", "LEFT", "RIGHT"};
-        boolean deadLock = false;
-        int minMoves = INFINITE_COST;
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        int directionIndex;
+        boolean[] deadlocks = {false, false, false, false};
+        Integer[] currentPosition = from;
+        ArrayList<Integer[]> possiblePositions;
+        boolean moved;
 
-        while (!deadLock) {
-            for (String direction: directions) {
-                switch (direction) {
-                    case "UP":
-                        break;
-                    case "DOWN":
-                        break;
-                    case "LEFT":
-                        break;
-                    case "RIGHT":
-                        break;
-                }
-            }
+        Map<Integer, Set<Integer[]>> paths = new HashMap<>();
+        for (int j = 0; j < dx.length; j++) {
+            paths.put(j, new HashSet<>());
         }
 
-        return minMoves;
+        /* index que nos indica en que dirección arrancó el recorrido */
+        directionIndex = 0;
+        while (directionIndex < 4 && !deadlocks[directionIndex]) {
+
+            possiblePositions = new ArrayList<>();
+            moved = false;
+            for (int i = 0; i < dx.length; i++){
+
+                /* primero chequeamos que la caja se pueda empujar en la dirección i */
+                Integer[] pushFrom = new Integer[]{-1, -1};
+                switch (i)  {
+                    case 0: pushFrom = new Integer[]{currentPosition[0] + dx[1], currentPosition[1] + dy[1]}; break;
+                    case 1: pushFrom = new Integer[]{currentPosition[0] + dx[0], currentPosition[1] + dy[0]}; break;
+                    case 2: pushFrom = new Integer[]{currentPosition[0] + dx[3], currentPosition[1] + dy[3]}; break;
+                    case 3: pushFrom = new Integer[]{currentPosition[0] + dx[2], currentPosition[1] + dy[2]}; break;
+                    default:break;
+                }
+                if (pushFrom[0] != -1 && b[pushFrom[0]][pushFrom[1]] != SquareType.TILE.getIcon()) {
+                    break;
+                }
+
+                /* despues nos fijamos si nos podemos posicionar en la nueva posición */
+                Integer[] nextPostion = {currentPosition[0] + dx[i], currentPosition[1] + dy[i]};
+
+                if (b[nextPostion[0]][nextPostion[1]] == SquareType.GOAL.getIcon()) {
+
+                    /* conseguimos un recorrido hacia el goal*/
+                    moved = true;
+                    currentPosition = nextPostion;
+                    Set<Integer[]> positions = paths.get(directionIndex);
+                    positions.add(currentPosition);
+                    paths.replace(directionIndex, positions);
+
+                    /* forma de decir que puede parar de buscar un recorrido en esta dirección */
+                    deadlocks[directionIndex] = true;
+                    currentPosition = from;
+                    directionIndex++;
+                    break;
+                }
+
+                if (b[nextPostion[0]][nextPostion[1]] == SquareType.TILE.getIcon()) {
+                    if (!paths.get(directionIndex).contains(nextPostion)) possiblePositions.add(nextPostion);
+                }
+
+            }
+
+            if (!moved && !possiblePositions.isEmpty()) {
+
+                int minDistanceIndex = -1;
+                int minDistance = INFINITE_COST.get();
+                int aux;
+                for (int k = 0; k < possiblePositions.size(); k++) {
+                    aux = manhattanDistance(possiblePositions.get(k), to);
+                    if (aux <= minDistance) {
+                        minDistance = aux;
+                        minDistanceIndex = k;
+                    }
+                }
+
+                currentPosition = possiblePositions.remove(minDistanceIndex);
+                moved = true;
+                Set<Integer[]> positions = paths.get(directionIndex);
+                positions.add(currentPosition);
+                paths.replace(directionIndex, positions);
+            }
+
+            if (!moved) {
+                paths.replace(directionIndex, new HashSet<>()); /* -> de esta manera indicamos que hubo un deadlock */
+                deadlocks[directionIndex] = true;
+                currentPosition = from;
+                directionIndex++;
+            }
+
+        }
+
+        AtomicInteger minMoves = INFINITE_COST;
+
+        paths.forEach((direction, positions) -> {
+            minMoves.set(Math.min(minMoves.get(), positions.size() == 0 ? INFINITE_COST.get() : positions.size()));
+        });
+
+        return minMoves.get();
     }
 
 }
